4728274c512c1840ae08599095c779d5d9881e99
Ying Wang <yvonnew104@gmail.com>
2018-03-22 23:09:09 -0700

    Add files via upload

---
+++ Solver.py
@@ -0,0 +1,176 @@
+from network import *
+import time
+import copy
+import numpy as np
+import random
+import os
+from torch.autograd import Variable
+import matplotlib
+matplotlib.use('TkAgg')
+from matplotlib import pyplot as plt
+import torch.nn.init as init
+
+class Solver(object):
+    def __init__(self, model, optim, loss_function, data, batch_size, epoch, reg_rate=0.2,
+                 save_path=os.path.join('model', 'new_model')):
+
+        self.model = model
+        self.X_train = data['X_train']
+        self.X_val = data['X_val']
+        self.X_test = data['X_test']
+        self.y_train = data['y_train']
+        self.y_val = data['y_val']
+        self.y_test = data['y_test']
+        self.batch_size = batch_size
+        self.epoch_num = epoch
+        self.num_class = 4
+        self.reg_rate = reg_rate
+        self.optim = optim
+        self.loss = loss_function
+        self.save_path = save_path
+        self.best_model = copy.deepcopy(model)
+
+        self.train_acc_his = []
+        self.val_acc_his = []
+        self.train_loss_his = []
+        self.save_check_epoch = 20
+
+        self.initial_model()
+
+    def initial_model(self):
+        for m in self.model.modules():
+            if isinstance(m, nn.Conv2d):
+                init.xavier_uniform(m.weight)
+                init.constant(m.bias, 0)
+            elif isinstance(m, nn.BatchNorm2d):
+                init.constant(m.weight, 1)
+                init.constant(m.bias, 0)
+            elif isinstance(m, nn.Linear):
+                init.normal(m.weight, std=1e-3)
+                init.constant(m.bias, 0)
+
+    def calculate_acc(self, y_pred_vector, y_label):
+        y_pred = np.argmax(y_pred_vector.cpu().data.numpy(), axis=1)
+        accuracy = np.sum(y_pred == y_label) / float(y_label.size)
+        return accuracy
+
+    def get_val_acc(self):
+        y_pred_vector = self.model.forward(Variable(Tensor(self.X_val)))
+        val_acc = self.calculate_acc(y_pred_vector, self.y_val)
+        return val_acc
+
+    def get_test_acc(self):
+        y_pred_vector = self.best_model.forward(Variable(Tensor(self.X_test)))
+        test_acc = self.calculate_acc(y_pred_vector, self.y_test)
+        return test_acc
+
+    def get_reg_loss(self):
+        reg_loss = 0.0
+        if self.reg_rate:
+            for W in self.model.parameters():
+                reg_loss += torch.sum(W * W)
+        return reg_loss*self.reg_rate
+
+    def train(self, random_choice=False):
+        best_val_accuracy = 0.0
+        data_len = self.y_train.size
+        batch_num = data_len // self.batch_size
+
+        plt.ion()
+        for epoch in range(0, self.epoch_num):
+            epoch_start_time = time.time()
+            for batch_epoch in range(0, batch_num):
+
+                if random_choice:
+                    choice_idnex = random.sample(range(0, data_len), self.batch_size)
+                    batch_train = self.X_train[choice_idnex]
+                    batch_label = self.y_train[choice_idnex]
+                else:
+                    batch_epoch_start = self.batch_size * batch_epoch
+                    batch_epoch_end = batch_epoch_start + self.batch_size
+
+                    batch_train = self.X_train[batch_epoch_start: batch_epoch_end]
+                    batch_label = self.y_train[batch_epoch_start: batch_epoch_end]
+
+                onehot_label = np.zeros((self.batch_size, self.num_class))
+                onehot_label[np.arange(self.batch_size), batch_label] = 1
+
+                batch_train, batch_label_onehot = Variable(Tensor(batch_train)), Variable(Tensor(onehot_label))
+                y_pred_vector = self.model.forward(batch_train)
+
+                reg_loss = self.get_reg_loss()
+                train_loss = self.loss(y_pred_vector, batch_label_onehot) + reg_loss
+
+                val_acc = self.get_val_acc()
+                if val_acc > best_val_accuracy:
+                    best_val_accuracy = val_acc
+                    self.best_model = copy.deepcopy(self.model)
+
+                self.optim.zero_grad()
+                train_acc = self.calculate_acc(y_pred_vector, batch_label)
+                train_loss.backward()
+                self.optim.step()
+
+                self.train_acc_his.append(train_acc)
+                self.val_acc_his.append(val_acc)
+                self.train_loss_his.append(float(train_loss))
+
+            if not epoch % self.save_check_epoch:
+                self.save_model()
+            # self.print_figure()
+            epoch_end_time = time.time()
+
+            print('Epoch: ', epoch, ' |training loss %.4f' % train_loss, ' |train_acc %.4f' % train_acc,
+                  ' |best validation acc : %.4f' % best_val_accuracy, ' |cost %.4f seconds' % (epoch_end_time - epoch_start_time))
+        plt.ioff()
+        # self.save_figure()
+        self.save_model()
+
+    def print_figure(self):
+        plt.figure(1, figsize=(20, 10))
+        plt.clf()
+        plt.title('Accuracy over steps')
+        plt.xlabel('epoch')
+        plt.ylabel('accuracy')
+        plt.plot(self.train_acc_his)
+        plt.plot(self.val_acc_his)
+        plt.legend(['train_accuracy', 'val_accuracy'])
+
+        plt.figure(2, figsize=(20, 10))
+        plt.clf()
+        plt.title('Training loss over steps')
+        plt.xlabel('epoc')
+        plt.ylabel('training loss')
+        plt.plot(self.train_loss_his)
+        plt.legend(['training_loss'])
+        plt.pause(0.1)
+        plt.show()
+
+    def save_model(self):
+        if not os.path.exists(self.save_path):
+            os.mkdir(self.save_path)
+        save_file = os.path.join(self.save_path, 'network_model.dat')
+        torch.save(self.best_model, save_file)
+        print('best model saved in ', save_file)
+
+    def save_figure(self):
+
+        plt.figure(1, figsize=(20, 10))
+        plt.clf()
+        plt.title('Accuracy over epoch')
+        plt.xlabel('epoch')
+        plt.ylabel('accuracy')
+        plt.plot(self.train_acc_his)
+        plt.plot(self.val_acc_his)
+        plt.legend(['train_accuracy', 'test_accuracy'])
+        plt.savefig(os.path.join(self.save_path, 'accuracy.png'))
+
+        plt.figure(2, figsize=(20, 10))
+        plt.clf()
+        plt.title('Training loss over epco')
+        plt.xlabel('epoch')
+        plt.ylabel('training loss')
+        plt.plot(self.train_loss_his)
+        plt.legend(['training_loss'])
+        plt.savefig(os.path.join(self.save_path, 'loss.png'))
+
---
+++ preprocess.py
@@ -0,0 +1,74 @@
+import os
+import h5py
+import numpy as np
+import torch
+
+def one_hot(y_):
+    y_ = y_.reshape(len(y_))
+    n_values = np.max(y_) + 1
+    return np.eye(n_values)[np.array(y_, dtype=np.int32)]
+
+def load_data(data_dir, data_size=9, train_proportion=0.8, num_test=50, data_shape=[-1, 1, 22, 1000]):
+    data = {}
+    for i in range(data_size):
+        data_name = [fn for fn in os.listdir(data_dir) if fn.startswith('A0%d'%(i+1)) and fn.endswith('.mat')]
+        assert len(data_name) == 1
+
+        data_path = os.path.join(data_dir, data_name[0])
+        A0xT = h5py.File(data_path, 'r')
+        X_A0xT = np.copy(A0xT['image'])[:, 0:22, :]
+        y_A0xT = np.copy(A0xT['type'])[0, 0:X_A0xT.shape[0]:1]
+
+        # remove trails contains nan
+        index = [c for c in range(X_A0xT.shape[0]) if ~np.any(np.isnan(X_A0xT[c]))]
+        X_A0xT = X_A0xT[index]
+        y_A0xT = y_A0xT[index]
+        y_A0xT = np.asarray(y_A0xT, dtype=np.int32)
+        
+        y_A0xT = y_A0xT-769
+
+        # split train, val, test
+        if i == 0:
+            data = split_data(X_A0xT, y_A0xT, train_proportion, num_test)
+        else:
+            sub_data = split_data(X_A0xT, y_A0xT, train_proportion, num_test)
+            for key, value in data.items():
+                data[key] = np.concatenate((data[key], sub_data[key]), axis=0)
+
+    # normalize dataset
+    # full_dataset = np.concatenate((data['X_train'], data['X_val'], data['X_test']), axis=0)
+    # mean = np.mean(full_dataset, axis=0)
+    # std = np.std(full_dataset, axis=0)
+    # data['X_train'] = (data['X_train'] - mean) / std
+    # data['X_val'] = (data['X_val'] - mean) / std
+    # data['X_test'] = (data['X_test'] - mean) / std
+
+    # shuffle train data
+    data['X_train'], data['y_train'] = shuffle(data['X_train'], data['y_train'])
+
+    data['X_train'] = data['X_train'].reshape((data_shape))
+    data['X_val'] = data['X_val'].reshape((data_shape))
+    data['X_test'] = data['X_test'].reshape((data_shape))
+
+
+    return data
+
+def split_data(X, y, train_proportion=0.8, num_test=50):
+    num_train = int(train_proportion * (y.shape[0] - num_test))
+    X, y = shuffle(X, y)
+    X_train, X_val, X_test = X[num_test:num_train+num_test,:,:], X[num_train+num_test:,:,:], X[:num_test,:,:]
+    y_train, y_val, y_test = y[num_test:num_train+num_test], y[num_train+num_test:], y[:num_test]
+
+    sub_data = {'X_train': X_train, 'X_val': X_val, 'X_test': X_test, 
+            'y_train': y_train, 'y_val': y_val, 'y_test': y_test}
+
+    return sub_data
+
+def shuffle(X, y):
+    index = np.arange(y.shape[0])
+    np.random.shuffle(index)
+    X = X[index,:,:]
+    y = y[index]
+    return X, y
+
+
